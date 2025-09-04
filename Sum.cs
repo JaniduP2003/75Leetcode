538e13342aeb2048d80fdd162408f088df308b75
Janidu Pabasara <141099578+JaniduP2003@users.noreply.github.com>
2025-09-04 12:29:28 +0530

    FIX:unnnesory code part
    i alrady gets the inputs and outputs i donr agin need to add the inputs in a loop
    
    removed XX
    for (int i = 0; i < arry.Length; i++)
    {
        arry[i] = int.Parse(Console.ReadLine());
    }

--- Sum.cs
+++ Sum.cs
@@ -4,30 +4,17 @@ namespace Sum
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-
             string num = Console.ReadLine();
 
-           string[] parts =num.Split(' ');  //splits the input in to part
+            string[] parts = num.Split(' ');  //splits the input in to part
 
 
             int[] arry = Array.ConvertAll(parts, int.Parse); //all the parts are now string in this line convertetd to int 
-             
-            
-            for (int i = 0; i < arry.Length; i++)
-            {
-                //readline 
-                arry[i] = int.Parse(Console.ReadLine());
-            }
-
-            
-               
-                //Console.WriteLine(arry[i]);
-                  Console.WriteLine("[" + string.Join(",", arry) + "]");
-
-
-            
+                                                             //Console.WriteLine(arry[i]);
+            Console.WriteLine("[" + string.Join(",", arry) + "]");
         }
     }
 }
