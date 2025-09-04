e8e4c5e67fe93e5edbcd4f9f1c69096ff024bda6
Janidu Pabasara <141099578+JaniduP2003@users.noreply.github.com>
2025-09-04 16:44:26 +0530

    Add :::: logic to find the sum

--- Sum.cs
+++ Sum.cs
@@ -7,6 +7,7 @@ namespace Sum
 
         static void Main(string[] args)
         {
+            int target = int.Parse(Console.ReadLine());
             string num = Console.ReadLine();
 
             string[] parts = num.Split(' ');  //splits the input in to part
@@ -15,6 +16,21 @@ namespace Sum
             int[] arry = Array.ConvertAll(parts, int.Parse); //all the parts are now string in this line convertetd to int 
                                                              //Console.WriteLine(arry[i]);
             Console.WriteLine("[" + string.Join(",", arry) + "]");
+
+            for (int i = 0; i < arry.Length; i++)
+            {
+                for (int j = i + 1; j < arry.Length; j++)
+                {
+                    if (arry[i] + arry[j] == target)
+                    {
+                        i += 1;
+                        j += 1;
+                        Console.WriteLine("[" + i + "," + j + "]");
+                    }
+                }
+            }
         }
+
+
     }
 }
