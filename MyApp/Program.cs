27f98d954ecd5572837e0589fedc0c07b6669096
Janidu Pabasara <141099578+JaniduP2003@users.noreply.github.com>
2025-09-07 06:20:48 +0530

    FIX:: in dibugger the ( is allways ceacked and it dose not get oust thts why all ways true becose it never pushes eneything even if dose it pop it back only chaking the first iteration

---
+++ MyApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sum
+{
+    class Valid_Parentheses
+    {
+
+        static void Main(string[] args)
+        {
+            //int target = int.Parse(Console.ReadLine());
+            //Console.WriteLine(target);
+
+            string input = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+
+
+
+
+            foreach (char number in input)
+            {
+                if (number == '(' || number == '{' || number == '[')
+                {
+                    stack.Push(number);
+                } 
+               
+              
+               if (number == ')' || number == '}' || number == ']')
+                    {
+                        stack.Pop();
+                    }
+                   
+            }
+
+             
+             if (stack.Count == 0)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("fulse");
+            }
+
+            // foreach (var item in stack)
+            // {Console.WriteLine(item);}
+            //Console.WriteLine(stack.Pop());
+
+        }
+    }
+}
